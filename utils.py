8ec93ec0870a812ac4ea7bf6f9a3c7f794adc035
Han Qiao <qiao@basis-ai.com>
2021-04-11 15:07:16 +0800

    Add typing to params dataclass

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 import numpy as np
 import torch
@@ -25,56 +26,56 @@ class HParams:
     ################################
     # Experiment Parameters        #
     ################################
-    epochs = 500
-    iters_per_checkpoint = 1000
-    seed = 1234
-    fp16_run = False
-    cudnn_enabled = True
-    cudnn_benchmark = False
-    ignore_layers = ["embedding.weight"]
+    epochs: int = 500
+    iters_per_checkpoint: int = 1000
+    seed: int = 1234
+    fp16_run: bool = False
+    cudnn_enabled: bool = True
+    cudnn_benchmark: bool = False
+    ignore_layers: List[str] = field(default_factory=lambda: ["embedding.weight"])
     ################################
     # Data Parameters             #
     ################################
-    training_files = "filelists/ljs_audio_text_train_filelist.txt"
-    validation_files = "filelists/ljs_audio_text_val_filelist.txt"
-    text_cleaners = ["english_cleaners"]
+    training_files: str = "filelists/ljs_audio_text_train_filelist.txt"
+    validation_files: str = "filelists/ljs_audio_text_val_filelist.txt"
+    text_cleaners: List[str] = field(default_factory=lambda: ["english_cleaners"])
     ################################
     # Landmark Parameters          #
     ################################
-    n_mel_channels = 204
+    n_mel_channels: int = 204
     ################################
     # Model Parameters             #
     ################################
-    n_symbols = len(symbols)
-    symbols_embedding_dim = 512
+    n_symbols: int = len(symbols)
+    symbols_embedding_dim: int = 512
     # Encoder parameters
-    encoder_kernel_size = 5
-    encoder_n_convolutions = 3
-    encoder_embedding_dim = 512
+    encoder_kernel_size: int = 5
+    encoder_n_convolutions: int = 3
+    encoder_embedding_dim: int = 512
     # Decoder parameters
-    n_frames_per_step = 1  # currently only 1 is supported
-    decoder_rnn_dim = 1024
-    prenet_dim = 256
-    max_decoder_steps = 1000
-    gate_threshold = 0.5
-    p_attention_dropout = 0.1
-    p_decoder_dropout = 0.1
+    n_frames_per_step: int = 1  # currently only 1 is supported
+    decoder_rnn_dim: int = 1024
+    prenet_dim: int = 256
+    max_decoder_steps: int = 1000
+    gate_threshold: float = 0.5
+    p_attention_dropout: float = 0.1
+    p_decoder_dropout: float = 0.1
     # Attention parameters
-    attention_rnn_dim = 1024
-    attention_dim = 128
+    attention_rnn_dim: int = 1024
+    attention_dim: int = 128
     # Location Layer parameters
-    attention_location_n_filters = 32
-    attention_location_kernel_size = 31
+    attention_location_n_filters: int = 32
+    attention_location_kernel_size: int = 31
     # Mel-post processing network parameters
-    postnet_embedding_dim = 512
-    postnet_kernel_size = 5
-    postnet_n_convolutions = 5
+    postnet_embedding_dim: int = 512
+    postnet_kernel_size: int = 5
+    postnet_n_convolutions: int = 5
     ################################
     # Optimization Hyperparameters #
     ################################
-    use_saved_learning_rate = False
-    learning_rate = 1e-3
-    weight_decay = 1e-6
-    grad_clip_thresh = 1.0
-    batch_size = 64
-    mask_padding = True  # set model's padded outputs to padded values
+    use_saved_learning_rate: bool = False
+    learning_rate: float = 1e-3
+    weight_decay: float = 1e-6
+    grad_clip_thresh: float = 1.0
+    batch_size: int = 64
+    mask_padding: bool = True  # set model's padded outputs to padded values
