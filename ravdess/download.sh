73396ae3f5e8ad8afb7ab4755db0f6313ada976a
Qiao Han <qiao@supabase.io>
2023-03-05 12:59:48 +0800

    Start ravdess download from first actor

--- ravdess/download.sh
+++ ravdess/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl --parallel-max 3 \
--Z 'https://zenodo.org/record/1188976/files/Video_Speech_Actor_[17-24].zip?download=1' \
+-Z 'https://zenodo.org/record/1188976/files/Video_Speech_Actor_[01-24].zip?download=1' \
 -o 'ravdess/Speech_Actor_#1.zip'
