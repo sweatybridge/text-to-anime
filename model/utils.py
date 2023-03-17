8ce0d7fb106d89f73cf1fe219dd65b94e88bb1de
fion-chai <99871669+fion-chai@users.noreply.github.com>
2023-03-17 10:20:22 +0800

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -58,7 +58,7 @@ class HParams:
     # Encoder parameters
     encoder_kernel_size: int = 5
     encoder_n_convolutions: int = 3
-    encoder_embedding_dim: int = 544
+    encoder_embedding_dim: int = 512
     
     # Decoder parameters
     n_frames_per_step: int = 1  # currently only 1 is supported
